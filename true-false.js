5cd0fa17d95eb9b69b22c3cf39828b7f0ef1dcb3
emranakanda <emranfate@gmail.com>
2020-03-04 13:24:28 +0600

    true and false difference

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//Falsy
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+
+//Truthy
+//"0", " " []
+
+let name = 12;
+
+if(name || name = 0){
+    console.log("this condition is true")
+}
+else{
+    console.log("this condition is false");
+}
